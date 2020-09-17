2166796631c5bb8d21e5b8c8d21dc09f6f28b01d
Austin <amarchese3@gmail.com>
2020-09-16 23:52:10 -0400

    Creating Topic Page Header Card

--- commons/buttons/FollowButton.js
+++ commons/buttons/FollowButton.js
@@ -1,39 +1,49 @@
-
 import React from 'react';
-import { Icon, Thumbnail, Text } from 'native-base';
-import { StyleSheet, View, Button, Image } from 'react-native';
+import {Icon, Thumbnail, Text, Button} from 'native-base';
+import {StyleSheet, View, Image} from 'react-native';
 import Animation from 'lottie-react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-export default class FollowButton  extends React.Component {
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import PropTypes from 'prop-types';
+export default class FollowButton extends React.Component {
   constructor(props) {
     super(props);
   }
-    render() {
-      return (
-      <View style={styles.container}>
-        <TouchableOpacity
-        onPress={() =>
-          alert('Following this thing')
-        }
-      >
-        <Thumbnail large source={require('../../assets/giants.png')} />
-        <Text style={styles.buttonText}>{this.props.name}</Text>
-        </TouchableOpacity>
+  render() {
 
-    </View>
+    let button;
+    if (this.props.isIconButton) {
+      button = <TouchableOpacity onPress={() => alert('Following this thing')}>
+        <Thumbnail large source={require('../../assets/giants.png')}/>
+        <Text style={styles.buttonText}>{this.props.text}</Text>
+      </TouchableOpacity>;
+    } else {
+      button = <Button rounded bordered small >
+        <Text>{this.props.text}</Text>
+      </Button>;
+    }
+
+    return (
+      <View style={styles.container}>
+        {button}
+      </View>
     );
   }
 
-  }
+}
 
+FollowButton.propTypes = {
+  text: PropTypes.string,
+  isIconButton: PropTypes.bool,
+};
+
+FollowButton.defaultProps = {
+  text: 'Follow',
+  isIconButton: false,
+};
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
   buttonText: {
-    textAlign:'center',
+    textAlign: 'center',
   },
 });
-
-
--- commons/buttons/TagButton.js
+++ commons/buttons/TagButton.js
@@ -1,30 +1,24 @@
 
 import React from 'react';
-import { Icon, Thumbnail, Text } from 'native-base';
-import { StyleSheet, View, Button, Image } from 'react-native';
+import { Icon, Thumbnail, Text, Button } from 'native-base';
+import { StyleSheet, View,  Image } from 'react-native';
 import Animation from 'lottie-react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default class FollowButton  extends React.Component {
+export default class TagButton  extends React.Component {
   constructor(props) {
     super(props);
   }
     render() {
-      return (
-      <View style={styles.container}>
-        <TouchableOpacity
-        onPress={() =>
-          alert('Following this thing')
-        }
-      >
-        <Text style={styles.tagButtonStyle}>{this.props.name}</Text>
-        </TouchableOpacity>
-
+  return (
+    <View style={styles.container}>
+      <Button style={styles.tagButtonStyle} rounded small light>
+      <Text>{this.props.name}</Text>
+    </Button>
     </View>
-    );
+  );
   }
-
-  }
+}
 
 
 const styles = StyleSheet.create({
